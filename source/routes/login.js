0009be5a4db39203849b1bd9b967fefe00f1135e
krishna_dixit <rkdixit96@gmail.com>
2018-03-03 20:35:41 +0530

    Hapi server boilerplate created

---
+++ source/routes/login.js
@@ -0,0 +1,11 @@
+module.exports = [{
+  method: 'POST',
+  path: '/login',
+  handler: (request, response) => {
+    // Check if questions are loaded into db
+    // If not -get question and store
+    // Check if user exists
+    // if not create user and send questions
+    // if exists send questions and user options
+  },
+}];
